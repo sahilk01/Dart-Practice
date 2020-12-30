509e8fbc7845a6113e6d45034c8086e20e287910
sahil <sahil.khan@eduaid.co.in>
2020-12-30 17:01:28 +0530

    practiced functions within the class

--- variable/variable_and_class.dart
+++ variable/variable_and_class.dart
@@ -4,13 +4,24 @@ main() {
   Profile profile = new Profile("Sahil Khan", 5);
   print("my firstName is: $firstName");
   print("my firstName is: ${profile.age}");
+  profile.writeSomething();
 }
 
 class Profile{
   int age;
   String name;
   Profile(String name, int age){
+
+    print("This is constructor of profile class");
     this.name = name;
     this.age = age;
+  }
+
+  writeSomething(){
+    print("Hi the number is ${getNumber()}");
+  }
+
+  int getNumber(){
+    return 5;
   }
 }
