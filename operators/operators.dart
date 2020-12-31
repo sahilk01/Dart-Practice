9d75f8cd8b63ce71cb94c8f6f9b22ce25593638c
sahil <sahil.khan@eduaid.co.in>
2020-12-31 10:32:49 +0530

    checking object type

--- operators/operators.dart
+++ operators/operators.dart
@@ -6,40 +6,8 @@ main(){
 
   print(a);
   yoyu();
+  checkingObjectType();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 yoyu(){
@@ -47,4 +15,18 @@ yoyu(){
   var isthat = a == "asdasd" ? "sadsad": "asdads";
 
   print(isthat);
+}
+
+checkingObjectType(){
+  print("Checking object type here");
+  var a = SahilClass();
+  if (a is SahilClass){
+    print("this is Sahil Class");
+  } else {
+    print("this is not Sahil Class");
+  }
+}
+
+class SahilClass{
+
 }
